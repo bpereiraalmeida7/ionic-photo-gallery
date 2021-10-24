4ecd829fdab4cfe43cee3dbe84a875ecce8ed519
Bianca Pereira de Almeida <bpereiraalmeida@gmail.com>
2021-10-23 22:33:35 -0300

    exibindo as fotos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
-
+import { Photo } from '../models/Photo';
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
+  public photos: Photo[] = [];
 
   constructor() { }
 
@@ -16,6 +17,11 @@ export class PhotoService {
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
     });
   }
 }
